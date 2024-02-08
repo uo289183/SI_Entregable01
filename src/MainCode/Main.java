1718226b6d25997f48615fe72dcbe13edcd5c94d
P4blo003 <pablogonzalezgarcia003@gmail.com>
2024-02-08 16:08:32 +0100

    Cuarta edición de la clase Main en master.

--- src/MainCode/Main.java
+++ src/MainCode/Main.java
@@ -12,6 +12,8 @@ public class Main {
 		System.out.println("Soy un error hecho a proposito jejejeje.");
 		errorValue = 1;
 		
+		System.out.println("Suy un amiguete que te ha solucionado el error.");
+		
 		System.out.println("Programa <Entregable01> finalizado con salida de error [" + errorValue + "].");
 
 	}
